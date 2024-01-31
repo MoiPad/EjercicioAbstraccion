a2b399d4e5b86ed78899316b4a21800c06102cf9
MoiPad92 <moipad85@gmail.com>
2024-01-30 19:40:16 -0600

    se agregaron dos parametros basicos a la clase base Creación de clase hija Estación

---
+++ EjercicioAbstraccion/AccesPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstraccion
+{
+    internal abstract class AccesPoint
+    {
+        public string Nombre { get; set; }
+        public string Direccion_IP { get; set;}
+        public string MacAddress { get; set; }
+        public string Modelo { get; set; }
+        public string ModoInalambrico { get; set; }
+        public string ModoRed { get; set; }
+        public string Gateway { get; set; }
+        public string SubnetMask { get; set; }
+        public int Estaciones { get; set; } // clientes asociados
+
+        public string pool = "9.0.0.254";// ip default para comparar
+        public abstract void Imprimir();
+        public abstract string Validacion();// activo o no
+        public abstract string Validacion_IP();// validad si la ip esta correcta
+    }
+}
